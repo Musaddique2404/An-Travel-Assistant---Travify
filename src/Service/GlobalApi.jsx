326a952136e743e6308b3c2a63e7fc8d67aaf7f1
Musaddique khan <161850281+Musaddique2404@users.noreply.github.com>
2025-12-16 12:36:34 +0300

    Refactor getCityDetails function for clarity

--- src/Service/GlobalApi.jsx
+++ src/Service/GlobalApi.jsx
@@ -19,6 +19,7 @@ const configPlace = {
     ],
   },
 };
+
 const configCity = {
   headers: {
     "Content-Type": "application/json",
@@ -39,4 +40,6 @@ export const PHOTO_URL =
 
 export const getPlaceDetails = (data) =>
   axios.post(BASE_URL, data, configPlace);
-export const getCityDetails = (data) => axios.post(BASE_URL, data, configCity);
+
+export const getCityDetails = (data) =>
+  axios.post(BASE_URL, data, configCity);
